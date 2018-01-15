812c646685f7c414618ebf5b9a23145c225b904c
Krishnan Rajiyah <krajiyah@gmail.com>
2018-01-15 02:32:29 -0800

    force SSL

--- index.js
+++ index.js
@@ -6,9 +6,21 @@ const cookieParser = require("cookie-parser");
 const router = require("./routers/main.js");
 const validator = require('./routers/validator.js');
 const slack = require("./util/slack.js");
+const config = require("./config.json");
+
+// CONSTANTS
+const port = process.env.PORT || config.devPort;
 
 // SETUP
 var app = express();
+
+// NOTE: comment this out when running locally
+app.use(function(req, res, next) {
+  if (!req.secure)
+    return res.redirect('https://' + req.headers.host + req.url);
+  next();
+});
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -22,7 +34,6 @@ app.use(express.static("public"));
 
 // START SERVER
 slack.listen();
-const port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Server listening on port: " + port);
 });
