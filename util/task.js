3ee5135185dec125ebf8c1acc7dd8d9cbc37bdb1
Krishnan Rajiyah <krajiyah@gmail.com>
2018-02-14 11:08:05 -0800

    [feat] start jobs on boot boolean

--- util/task.js
+++ util/task.js
@@ -1,8 +1,10 @@
 // DEPENDENCIES
-var schedule = require('node-schedule-tz');
+const schedule = require('node-schedule-tz');
+const config = require("../config.json");
 
 // METHODS
 function runOnceADay(hourOfDay, fn) {
+  if (config.startJobsOnBoot === true) fn();
   schedule.scheduleJob("0 " + hourOfDay + " * * *", "America/Los_Angeles", fn);
 }
 
