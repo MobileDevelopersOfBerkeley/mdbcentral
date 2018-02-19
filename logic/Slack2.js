e2e1acbd60871144ad08fa3dccee82c7a29123b1
Krishnan Rajiyah <krajiyah@gmail.com>
2018-02-18 23:22:26 -0800

    [refactor] slack2 bot

--- logic/Slack2.js
+++ logic/Slack2.js
@@ -7,6 +7,16 @@ var util = require("../util/util.js");
 const SLACK_CHANNEL = process.env.SLACK_BOT2_CHANNEL1;
 const DAYS_AHEAD = 2;
 
+// HELPERS
+function _sendTaskReminder(task) {
+  return bot2.sendToChannel(
+    SLACK_CHANNEL,
+    "*" + task.recipient + "* you have to finish task *" +
+    task.id + "* in *" + task._daysApart +
+    " days*: " + task.info
+  )
+}
+
 // METHODS
 function listen(successCb, errorCb) {
   bot2.start().then(function(c) {
@@ -19,19 +29,13 @@ function listen(successCb, errorCb) {
 function sendReminders() {
   return jira.getTasks().then(function(tasks) {
     var today = new Date();
-    return Promise.all(tasks.map(function(task) {
+    tasks = tasks.map(function(task) {
       task._daysApart = util.daysApart(today, task.dueDate);
       return task;
     }).filter(function(task) {
       return task._daysApart <= DAYS_AHEAD;
-    }).map(function(task) {
-      return bot2.sendToChannel(
-        SLACK_CHANNEL,
-        "*" + task.recipient + "* you have to finish task *" +
-        task.id + "* in *" + task._daysApart +
-        " days*: " + task.info
-      )
-    }));
+    });
+    return Promise.all(tasks.map(_sendTaskReminder));
   });
 }
 
